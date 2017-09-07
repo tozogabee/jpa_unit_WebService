15edea9b664f1d17643b8cbcbb05254de35f066c
tozogabee <tozogabee@gmail.com>
2017-09-07 20:04:40 +0200

    Extended ActivityRepo

--- src/main/java/com/foldik/split/persistence/ActivityRepository.java
+++ src/main/java/com/foldik/split/persistence/ActivityRepository.java
@@ -1,8 +1,17 @@
 package com.foldik.split.persistence;
 
 import com.foldik.split.persistence.model.Activity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ActivityRepository extends CrudRepository<Activity, Long> {
-    
+
+    Iterable<Activity> findAllByStartTimeBetween(LocalDateTime start, LocalDateTime end);
+
+    @Query("select a from Activity a where (a.startTime between :start and :end) or (a.endTime between :start and :end)")
+    List<Activity> findActivitiesBetween(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 }
--- src/test/java/com/foldik/split/persistence/ActivityRepositoryTest.java
+++ src/test/java/com/foldik/split/persistence/ActivityRepositoryTest.java
@@ -10,7 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -29,16 +31,49 @@ public class ActivityRepositoryTest {
 
     @Test
     public void whenSavedOneActivity_thenShouldBePersisted() {
-        Activity activity = new Activity();
-        activity.setActivityName("Programming");
-        activity.setStartTime(LocalDateTime.parse("2017-09-07T17:30:00"));
-        activity.setEndTime(LocalDateTime.parse("2017-09-07T21:00:00"));
+        Activity activity = activity(
+                "Programming",
+                "2017-09-07T17:30:00",
+                "2017-09-07T21:00:00");
 
         underTest.save(activity);
 
         Iterable<Activity> result = underTest.findAll();
         Activity resultActivity = result.iterator().next();
         assertThat(resultActivity.getActivityName(), is("Programming"));
+    }
+
+    @Test
+    public void givenOneActivityBetweenPeriod_thenReturnIt() {
+        Activity activity1 = activity(
+                "Programming",
+                "2017-09-07T17:00:00",
+                "2017-09-07T18:00:00");
+        Activity activity2 = activity(
+                "Dance",
+                "2017-09-07T18:20:00",
+                "2017-09-07T19:00:00");
+        Activity activity3 = activity(
+                "Shower",
+                "2017-09-06T18:20:00",
+                "2017-09-06T19:00:00");
+        underTest.save(activity1);
+        underTest.save(activity2);
+        underTest.save(activity3);
+
+        List<Activity> result = underTest.findActivitiesBetween(
+                LocalDateTime.parse("2017-09-07T18:20:00"), LocalDateTime.parse("2017-09-07T19:00:00"));
+
+        assertThat(result, hasSize(0));
+        assertThat(result.get(0).getActivityName(), is("Dance"));
+    }
+
+    private Activity activity(String name, String start, String end) {
+        Activity activity = new Activity();
+        activity.setActivityName(name);
+        activity.setStartTime(LocalDateTime.parse(start));
+        activity.setEndTime(LocalDateTime.parse(end));
+        return activity;
     }
 
 }
